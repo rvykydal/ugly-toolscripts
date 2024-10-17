624e4536d84ba99cf2f4c0d9f018b4950793eb1a
Radek Vykydal <rvykydal@redhat.com>
2024-10-17 10:32:06 +0200

    pxeconf: add support for stage2 download

--- testing/pxeconf.sh
+++ testing/pxeconf.sh
@@ -15,6 +15,8 @@ pxecfg_dir="/var/lib/tftpboot/linux-install/pxelinux.cfg"
 machine_dir="${pxecfg_dir}/users.cfg/rv/machines"
 images_subdir="test/users/rv/scripted"
 images_dir="/var/lib/tftpboot/linux-install/${images_subdir}"
+stage2_dir="/mnt/data/users/rv/s2/scripted"
+
 
 while getopts ":dxt:m:b:e" opt; do
   case $opt in
@@ -70,15 +72,29 @@ if [[ $# -lt 1 && "${edit}" == "no" ]]; then
   exit 1
 fi
 
+# rv_test
+#mac="52:54:00:9f:21:28"
 case $machine in
   rv_test)
-    mac="52:54:00:9f:21:28"
+    mac="52:54:00:fc:b2:25"
     ;;
   rv_test2)
     mac="52:54:00:5d:bf:04"
     ;;
   rv_test3)
     mac="52:54:00:12:d8:4f"
+    ;;
+  rv_test_uefi)
+    mac="52:54:00:24:f0:9b"
+    ;;
+  rv_r9)
+    mac="52:54:00:00:3d:0e"
+    ;;
+  rv_btrfs_auto)
+    mac="52:54:00:ca:7d:f2"
+    ;;
+  rv_lvm_auto)
+    mac="52:54:00:6c:69:e5"
     ;;
   *)
     if [[ "$machine" =~ "^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$" ]]; then
@@ -97,7 +113,7 @@ esac
 mac_cfg=01-$(echo "$mac" | tr '[:upper:]' '[:lower:]' | tr ':' '-')
 
 
-cfg_file=${machine_dir}/${mac_cfg}
+cfg_file=${machine_dir}/${mac_cfg} 
 url=$1
 
 if [[ ${infer_target} == "yes" && "$url" =~ "\/([^\/]*)\/compose\/" ]]; then
@@ -147,7 +163,7 @@ else
 label ${target}
   menu label ${target}
   kernel ${images_subdir}/${target}/vmlinuz
-  append initrd=${images_subdir}/${target}/initrd.img repo=${url} ${bootopts}
+  append initrd=${images_subdir}/${target}/initrd.img inst.repo=${url} ${bootopts}
 EOF
 fi
 
@@ -163,6 +179,14 @@ echo "Downloading kernel and initrd."
 curl -L ${url}/images/pxeboot/vmlinuz --output ${target_images_dir}/vmlinuz
 curl -L ${url}/images/pxeboot/initrd.img --output ${target_images_dir}/initrd.img
 
+target_stage2_dir=${stage2_dir}/${target}/images
+if [ -d ${target_stage2_dir} ]; then
+    echo "Downloading install.img to ${target_stage2_dir}."
+    curl -L ${url}/images/install.img --output ${target_stage2_dir}/install.img
+else
+    echo "Not downloading install.img to ${target_stage2_dir} which does not exist."
+fi
+
 if [[ ${default} == "yes" ]]; then
   echo "Setting as default target."
   sed -i -e "/^DEFAULT/ s|.*|DEFAULT ${target}|" ${cfg_file}
@@ -173,7 +197,9 @@ egrep '^DEFAULT' ${cfg_file}
 
 symlink_file=${pxecfg_dir}/${mac_cfg}
 if [[ -e ${symlink_file} ]]; then
-  echo "WARNING: File ${symlink_file} exists, should be a symlink!"
+  if [[ ! -L ${symlink_file} ]]; then
+      echo "WARNING: File ${symlink_file} exists, should be a symlink!"
+  fi
 else
   if [[ ! -L ${symlink_file} ]]; then
     echo "Creating symlink ${symlink_file}"
