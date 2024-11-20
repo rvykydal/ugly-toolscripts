e80e4a937495a3e9c2b19c1fda47312ecde4ad1e
Radek Vykydal <rvykydal@redhat.com>
2024-11-20 13:06:54 +0100

    rv-virt: add upd-pykikcstart.sh script

---
+++ vm-testing/upd-pykickstart.sh
@@ -0,0 +1,17 @@
+rm -rf ./updates/*
+
+GIT_BRANCH=KKoukiou-certificates-download
+PYKICKSTART_GIT=/home/rvykydal/work/features/edns-certs/pykickstart
+UPDATES_DIR=updates/usr/lib/python3.13/site-packages/pykickstart
+
+mkdir -p ${UPDATES_DIR}/commands
+mkdir -p ${UPDATES_DIR}/handlers
+
+cd $PYKICKSTART_GIT
+git checkout $GIT_BRANCH
+cd -
+
+cp $PYKICKSTART_GIT/pykickstart/commands/autopart.py $UPDATES_DIR/commands/autopart.py
+cp $PYKICKSTART_GIT/pykickstart/handlers/f41.py $UPDATES_DIR/handlers/f41.py
+
+scripts/makeupdates -k
